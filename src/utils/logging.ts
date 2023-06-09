608df46c7ee367996a427fa712c87d1857124208
mattorp <matto@oeye.site>
2023-06-09 10:53:49 +0200

    add log util

---
+++ src/utils/logging.ts
@@ -0,0 +1,19 @@
+import * as vscode from 'vscode'
+
+export const outputChannel = vscode.window.createOutputChannel(
+  'mattorp.quokka-alias'
+)
+
+export const log = (...args: any[] | any) => {
+  const message = [args]
+    .flat()
+    .map(
+      (arg) =>
+        // Show message and stringified args
+        `${arg.message ? '\n' + arg.message : ''}\n${JSON.stringify(arg)}`
+    )
+    .join('')
+  console.log(message)
+  outputChannel.appendLine(message)
+  return args as typeof args
+}
